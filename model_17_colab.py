bd1b48624172332aaf8170b5085e43b9b1965a19
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-06-04 02:33:37 +0900

    Update model_17_colab.py

--- model_17_colab.py
+++ model_17_colab.py
@@ -66,25 +66,27 @@ def Unet3_plus_modified(input_shape=(512, 512, 3), nclasses=1):
     cls_branch = tf.nn.sigmoid(cls_branch)
     cls_branch_max = tf.keras.backend.max(cls_branch, -1)
     
-    max_pixel = -tf.math.log((1/(cls_branch_max + tf.keras.backend.epsilon()) ) - 1)
+    #max_pixel = -tf.math.log((1/(cls_branch_max + tf.keras.backend.epsilon()) ) - 1)
+    #max_pixel = max_pixel[:, tf.newaxis, tf.newaxis, tf.newaxis] # 이 부분이 문제인듯
+    # 이 부분때문에 nan이 뜨는것같음
     #####################################################################################
     h1_PT_hd4 = tf.keras.layers.MaxPool2D(8, 8)(h_1)
     h1_PT_hd4 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h1_PT_hd4)
     h_4_ = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h_4)
-    h_4_ = tf.where(tf.nn.sigmoid(h_4_) >= 0.5, h_4_ + max_pixel, h_4_ - max_pixel)
-    h1_PT_hd4 = tf.where(tf.nn.sigmoid(h1_PT_hd4) >= 0.5, h1_PT_hd4 + max_pixel, h1_PT_hd4 - max_pixel)
+    h_4_ = tf.where(tf.nn.sigmoid(h_4_) >= 0.5, h_4_ , 0.)
+    h1_PT_hd4 = tf.where(tf.nn.sigmoid(h1_PT_hd4) >= 0.5, h1_PT_hd4, 0.)
     h1_PT_hd4 = tf.keras.layers.BatchNormalization()(h1_PT_hd4 + h_4_)
     h1_PT_hd4 = tf.keras.layers.ReLU()(h1_PT_hd4)
 
     h2_PT_hd4 = tf.keras.layers.MaxPool2D(4, 4)(h_2)
     h2_PT_hd4 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h2_PT_hd4)
-    h2_PT_hd4 = tf.where(tf.nn.sigmoid(h2_PT_hd4) >= 0.5, h2_PT_hd4 + max_pixel, h2_PT_hd4 - max_pixel)
+    h2_PT_hd4 = tf.where(tf.nn.sigmoid(h2_PT_hd4) >= 0.5, h2_PT_hd4, 0.)
     h2_PT_hd4 = tf.keras.layers.BatchNormalization()(h2_PT_hd4 + h_4_)
     h2_PT_hd4 = tf.keras.layers.ReLU()(h2_PT_hd4)
 
     h3_PT_hd4 = tf.keras.layers.MaxPool2D(2, 2)(h_3)
     h3_PT_hd4 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h3_PT_hd4)
-    h3_PT_hd4 = tf.where(tf.nn.sigmoid(h3_PT_hd4) >= 0.5, h3_PT_hd4 + max_pixel, h3_PT_hd4 - max_pixel)
+    h3_PT_hd4 = tf.where(tf.nn.sigmoid(h3_PT_hd4) >= 0.5, h3_PT_hd4, 0.)
     h3_PT_hd4 = tf.keras.layers.BatchNormalization()(h3_PT_hd4 + h_4_)
     h3_PT_hd4 = tf.keras.layers.ReLU()(h3_PT_hd4)
 
@@ -94,7 +96,7 @@ def Unet3_plus_modified(input_shape=(512, 512, 3), nclasses=1):
 
     hd5_UT_hd4 = tf.keras.layers.UpSampling2D(2, interpolation='bilinear')(h_5)
     hd5_UT_hd4 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd5_UT_hd4)
-    hd5_UT_hd4 = tf.where(tf.nn.sigmoid(hd5_UT_hd4) >= 0.5, hd5_UT_hd4 + max_pixel, hd5_UT_hd4 - max_pixel)
+    hd5_UT_hd4 = tf.where(tf.nn.sigmoid(hd5_UT_hd4) >= 0.5, hd5_UT_hd4, 0.)
     hd5_UT_hd4 = tf.keras.layers.BatchNormalization()(hd5_UT_hd4 + h_4_)
     hd5_UT_hd4 = tf.keras.layers.ReLU()(hd5_UT_hd4)
 
@@ -108,14 +110,14 @@ def Unet3_plus_modified(input_shape=(512, 512, 3), nclasses=1):
     h1_PT_hd3 = tf.keras.layers.MaxPool2D(4, 4)(h_1)
     h1_PT_hd3 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h1_PT_hd3)
     h_3_ = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h_3)
-    h_3_ = tf.where(tf.nn.sigmoid(h_3_) >= 0.5, h_3_ + max_pixel, h_3_ - max_pixel)
-    h1_PT_hd3 = tf.where(tf.nn.sigmoid(h1_PT_hd3) >= 0.5, h1_PT_hd3 + max_pixel, h1_PT_hd3 - max_pixel)
+    h_3_ = tf.where(tf.nn.sigmoid(h_3_) >= 0.5, h_3_, 0.)
+    h1_PT_hd3 = tf.where(tf.nn.sigmoid(h1_PT_hd3) >= 0.5, h1_PT_hd3, 0.)
     h1_PT_hd3 = tf.keras.layers.BatchNormalization()(h1_PT_hd3 + h_3_)
     h1_PT_hd3 = tf.keras.layers.ReLU()(h1_PT_hd3)
 
     h2_PT_hd3 = tf.keras.layers.MaxPool2D(2, 2)(h_2)
     h2_PT_hd3 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h2_PT_hd3)
-    h2_PT_hd3 = tf.where(tf.nn.sigmoid(h2_PT_hd3) >= 0.5, h2_PT_hd3 + max_pixel, h2_PT_hd3 - max_pixel)
+    h2_PT_hd3 = tf.where(tf.nn.sigmoid(h2_PT_hd3) >= 0.5, h2_PT_hd3, 0.)
     h2_PT_hd3 = tf.keras.layers.BatchNormalization()(h2_PT_hd3 + h_3_)
     h2_PT_hd3 = tf.keras.layers.ReLU()(h2_PT_hd3)
 
@@ -125,13 +127,13 @@ def Unet3_plus_modified(input_shape=(512, 512, 3), nclasses=1):
 
     hd4_UT_hd3 = tf.keras.layers.UpSampling2D(2, interpolation='bilinear')(hd4)
     hd4_UT_hd3 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd4_UT_hd3)
-    hd4_UT_hd3 = tf.where(tf.nn.sigmoid(hd4_UT_hd3) >= 0.5, hd4_UT_hd3 + max_pixel, hd4_UT_hd3 - max_pixel)
+    hd4_UT_hd3 = tf.where(tf.nn.sigmoid(hd4_UT_hd3) >= 0.5, hd4_UT_hd3,0.)
     hd4_UT_hd3 = tf.keras.layers.BatchNormalization()(hd4_UT_hd3 + h_3_)
     hd4_UT_hd3 = tf.keras.layers.ReLU()(hd4_UT_hd3)
 
     hd5_UT_hd3 = tf.keras.layers.UpSampling2D(4, interpolation='bilinear')(h_5)
     hd5_UT_hd3 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd5_UT_hd3)
-    hd5_UT_hd3 = tf.where(tf.nn.sigmoid(hd5_UT_hd3) >= 0.5, hd5_UT_hd3 + max_pixel, hd5_UT_hd3 - max_pixel)
+    hd5_UT_hd3 = tf.where(tf.nn.sigmoid(hd5_UT_hd3) >= 0.5, hd5_UT_hd3, 0.)
     hd5_UT_hd3 = tf.keras.layers.BatchNormalization()(hd5_UT_hd3 + h_3_)
     hd5_UT_hd3 = tf.keras.layers.ReLU()(hd5_UT_hd3)
 
@@ -145,8 +147,8 @@ def Unet3_plus_modified(input_shape=(512, 512, 3), nclasses=1):
     h1_PT_hd2 = tf.keras.layers.MaxPool2D(2, 2)(h_1)
     h1_PT_hd2 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same")(h1_PT_hd2)
     h_2_ = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h_2)
-    h_2_ = tf.where(tf.nn.sigmoid(h_2_) >= 0.5, h_2_ + max_pixel, h_2_ - max_pixel)
-    h1_PT_hd2 = tf.where(tf.nn.sigmoid(h1_PT_hd2) >= 0.5, h1_PT_hd2 + max_pixel, h1_PT_hd2 - max_pixel)
+    h_2_ = tf.where(tf.nn.sigmoid(h_2_) >= 0.5, h_2_, 0.)
+    h1_PT_hd2 = tf.where(tf.nn.sigmoid(h1_PT_hd2) >= 0.5, h1_PT_hd2, 0.)
     h1_PT_hd2 = tf.keras.layers.BatchNormalization()(h1_PT_hd2 + h_2_)
     h1_PT_hd2 = tf.keras.layers.ReLU()(h1_PT_hd2)
 
@@ -156,19 +158,19 @@ def Unet3_plus_modified(input_shape=(512, 512, 3), nclasses=1):
 
     hd3_UT_hd2 = tf.keras.layers.UpSampling2D(2, interpolation='bilinear')(hd3)
     hd3_UT_hd2 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd3_UT_hd2)
-    hd3_UT_hd2 = tf.where(tf.nn.sigmoid(hd3_UT_hd2) >= 0.5, hd3_UT_hd2 + max_pixel, hd3_UT_hd2 - max_pixel)
+    hd3_UT_hd2 = tf.where(tf.nn.sigmoid(hd3_UT_hd2) >= 0.5, hd3_UT_hd2, 0.)
     hd3_UT_hd2 = tf.keras.layers.BatchNormalization()(hd3_UT_hd2 + h_2_)
     hd3_UT_hd2 = tf.keras.layers.ReLU()(hd3_UT_hd2)
 
     hd4_UT_hd2 = tf.keras.layers.UpSampling2D(4, interpolation='bilinear')(hd4)
     hd4_UT_hd2 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd4_UT_hd2)
-    hd4_UT_hd2 = tf.where(tf.nn.sigmoid(hd4_UT_hd2) >= 0.5, hd4_UT_hd2 + max_pixel, hd4_UT_hd2 - max_pixel)
+    hd4_UT_hd2 = tf.where(tf.nn.sigmoid(hd4_UT_hd2) >= 0.5, hd4_UT_hd2, 0.)
     hd4_UT_hd2 = tf.keras.layers.BatchNormalization()(hd4_UT_hd2 + h_2_)
     hd4_UT_hd2 = tf.keras.layers.ReLU()(hd4_UT_hd2)
 
     hd5_UT_hd2 = tf.keras.layers.UpSampling2D(8, interpolation='bilinear')(h_5)
     hd5_UT_hd2 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd5_UT_hd2)
-    hd5_UT_hd2 = tf.where(tf.nn.sigmoid(hd5_UT_hd2) >= 0.5, hd5_UT_hd2 + max_pixel, hd5_UT_hd2 - max_pixel)
+    hd5_UT_hd2 = tf.where(tf.nn.sigmoid(hd5_UT_hd2) >= 0.5, hd5_UT_hd2, 0.)
     hd5_UT_hd2 = tf.keras.layers.BatchNormalization()(hd5_UT_hd2 + h_2_)
     hd5_UT_hd2 = tf.keras.layers.ReLU()(hd5_UT_hd2)
 
@@ -186,26 +188,26 @@ def Unet3_plus_modified(input_shape=(512, 512, 3), nclasses=1):
     hd2_UT_hd1 = tf.keras.layers.UpSampling2D(2, interpolation='bilinear')(hd2)
     hd2_UT_hd1 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd2_UT_hd1)
     h_1_ = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(h_1)
-    h_1_ = tf.where(tf.nn.sigmoid(h_1_) >= 0.5, h_1_ + max_pixel, h_1_ - max_pixel)
-    hd2_UT_hd1 = tf.where(tf.nn.sigmoid(hd2_UT_hd1) >= 0.5, hd2_UT_hd1 + max_pixel, hd2_UT_hd1 - max_pixel)
+    h_1_ = tf.where(tf.nn.sigmoid(h_1_) >= 0.5, h_1_, 0.)
+    hd2_UT_hd1 = tf.where(tf.nn.sigmoid(hd2_UT_hd1) >= 0.5, hd2_UT_hd1, 0.)
     hd2_UT_hd1 = tf.keras.layers.BatchNormalization()(hd2_UT_hd1 + h_1_)
     hd2_UT_hd1 = tf.keras.layers.ReLU()(hd2_UT_hd1)
 
     hd3_UT_hd1 = tf.keras.layers.UpSampling2D(4, interpolation='bilinear')(hd3)
     hd3_UT_hd1 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd3_UT_hd1)
-    hd3_UT_hd1 = tf.where(tf.nn.sigmoid(hd3_UT_hd1) >= 0.5, hd3_UT_hd1 + max_pixel, hd3_UT_hd1 - max_pixel)
+    hd3_UT_hd1 = tf.where(tf.nn.sigmoid(hd3_UT_hd1) >= 0.5, hd3_UT_hd1, 0.)
     hd3_UT_hd1 = tf.keras.layers.BatchNormalization()(hd3_UT_hd1 + h_1_)
     hd3_UT_hd1 = tf.keras.layers.ReLU()(hd3_UT_hd1)
 
     hd4_UT_hd1 = tf.keras.layers.UpSampling2D(8, interpolation='bilinear')(hd4)
     hd4_UT_hd1 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd4_UT_hd1)
-    hd4_UT_hd1 = tf.where(tf.nn.sigmoid(hd4_UT_hd1) >= 0.5, hd4_UT_hd1 + max_pixel, hd4_UT_hd1 - max_pixel)
+    hd4_UT_hd1 = tf.where(tf.nn.sigmoid(hd4_UT_hd1) >= 0.5, hd4_UT_hd1, 0.)
     hd4_UT_hd1 = tf.keras.layers.BatchNormalization()(hd4_UT_hd1 + h_1_)
     hd4_UT_hd1 = tf.keras.layers.ReLU()(hd4_UT_hd1)
 
     hd5_UT_hd1 = tf.keras.layers.UpSampling2D(16, interpolation='bilinear')(h_5)
     hd5_UT_hd1 = tf.keras.layers.Conv2D(filters=Catchannel, kernel_size=3, padding="same", use_bias=False)(hd5_UT_hd1)
-    hd5_UT_hd1 = tf.where(tf.nn.sigmoid(hd5_UT_hd1) >= 0.5, hd5_UT_hd1 + max_pixel, hd5_UT_hd1 - max_pixel)
+    hd5_UT_hd1 = tf.where(tf.nn.sigmoid(hd5_UT_hd1) >= 0.5, hd5_UT_hd1, 0.)
     hd5_UT_hd1 = tf.keras.layers.BatchNormalization()(hd5_UT_hd1 + h_1_)
     hd5_UT_hd1 = tf.keras.layers.ReLU()(hd5_UT_hd1)
 
