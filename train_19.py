f9c1cdc25abefd75bc90a20ec0c3aaafda4bb4be
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-06-08 14:04:10 +0900

    Update train_19.py

--- train_19.py
+++ train_19.py
@@ -192,6 +192,55 @@ def hybrid_loss(y_true, y_pred, alpha):
 
     return focal_loss + iou_loss + dice_loss
 
+def tversky_coef(y_true, y_pred, alpha=0.5, const=K.epsilon()):
+    '''
+    Weighted Sørensen Dice coefficient.
+    
+    Input
+    ----------
+        y_true, y_pred: predicted outputs and targets.
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
+        
+    '''
+    
+    # get true pos (TP), false neg (FN), false pos (FP).
+    true_pos  = tf.reduce_sum(y_true * y_pred)
+    false_neg = tf.reduce_sum(y_true * (1-y_pred))
+    false_pos = tf.reduce_sum((1-y_true) * y_pred)
+    
+    # TP/(TP + a*FN + b*FP); a+b = 1
+    coef_val = (true_pos + const)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + const)
+    
+    return coef_val # alpha part need to be fix!!!!!
+
+def focal_tversky(y_true, y_pred, alpha=0.5, gamma=4/3, const=K.epsilon()):
+    
+    '''
+    Focal Tversky Loss (FTL)
+    
+    focal_tversky(y_true, y_pred, alpha=0.5, gamma=4/3)
+    
+    ----------
+    Abraham, N. and Khan, N.M., 2019, April. A novel focal tversky loss function with improved 
+    attention u-net for lesion segmentation. In 2019 IEEE 16th International Symposium on Biomedical Imaging 
+    (ISBI 2019) (pp. 683-687). IEEE.
+    
+    ----------
+    Input
+        alpha: tunable parameter within [0, 1]. Alpha handles imbalance classification cases 
+        gamma: tunable parameter within [1, 3].
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
+        
+    '''
+    # tf tensor casting
+    y_pred = tf.convert_to_tensor(y_pred)
+    y_true = tf.cast(y_true, y_pred.dtype)
+    
+    
+    # (Tversky loss)**(1/gamma) 
+    loss_val = tf.math.pow((1-tversky_coef(y_true, y_pred, alpha=alpha, const=const)), 1/gamma)
+    
+    return loss_val
 
 def run_model(model, images, training=True):
     return model(images, training=training)
